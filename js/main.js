16ef0c4ad7752d32bc4e410573748be3e107a047
Jammy3782 <jammy3782@gmail.com>
2017-02-23 08:46:32 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
-/* Custom JS goes here. */
+// document ready handler.
 
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
+$(document).on('ready', function(){
+    // Form validation
+    $('#order-form').validate({
+        submitHandler: function(form) {
+     //Submit Form  
+            form.submit();
+        },
+// validation rules 
+              rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128,
+                digits: false
+            },
+            "your-state": {
+                required: true,
+                maxlength: 2
+                },
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            }
+        }
+    });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+});
 
-// Refer to the `index.html` file for the validation rules that must be enforced.
